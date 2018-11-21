ccd23e9b9781e31d74cd6f5d98c5c7beedcfe98a
Yuri Magalhaes <yurirobertocm@gmail.com>
2018-11-21 11:54:26 -0300

    Defeinindo rotas de administração de páginas

---
+++ templates/admin/pages/create.tpl.php
@@ -0,0 +1,5 @@
+<h3>Administração de páginas</h3>
+
+//criação de página
+
+<a href="/admin">Ir para home</a>
---
+++ templates/admin/pages/delete.tpl.php
@@ -0,0 +1,5 @@
+<h3>Administração de páginas</h3>
+
+//remoção
+
+<a href="/admin">Ir para home</a>
