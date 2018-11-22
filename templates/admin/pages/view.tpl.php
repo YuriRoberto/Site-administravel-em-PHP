c5273065bdf0f178514c95c24a3a8c5963739b05
Yuri Magalhaes <yurirobertocm@gmail.com>
2018-11-22 15:51:45 -0300

    Visualização de detalhes de página

--- templates/admin/pages/view.tpl.php
+++ templates/admin/pages/view.tpl.php
@@ -1,5 +1,34 @@
-<h3>Administração de páginas</h3>
+<h3 class="mb-5">Administração de páginas</h3>
 
-//visualização
+<div class="row">
+    <div class="col-3">
+        <dl class="row">
+            <dt class="col-sm-6">Título</dt>
+            <dd class="col-sm-10">Página inicial</dd>
+            
+            <dt class="col-sm-6">URL</dt>
+            <dd class="col-sm-10">/ - <a href="/" target="blank">abrir</a></dd>
 
-<a href="/admin">Ir para home</a>
+            <dt class="col-sm-6">Criado em</dt>
+            <dd class="col-sm-10">2018-01-01 01:01</dd>
+
+            <dt class="col-sm-6">Atualizado em</dt>
+            <dd class="col-sm-10">2019-01-01</dd>
+
+        </dl>
+
+    </div>
+    <div class="col bg-light">
+        <h3>Página inicial</h3>
+        <p>Está é a página inicial do site </p>
+
+    </div>
+</div>
+
+<p>
+    <a href="/admin/pages/1/edit" class="btn btn-primary">editar</a>
+    <a href="/admin/pages/1/delete" class="btn btn-danger">remover</a>
+
+</p>
+
+<a href="/admin/pages" class="btn btn-secondary">Voltar</a>
