bb376bd3238df8385d055412f62b700c50efaee7
Yuri Magalhaes <yurirobertocm@gmail.com>
2018-11-23 10:05:42 -0300

    Formulário de edição de página

--- templates/admin/pages/edit.tpl.php
+++ templates/admin/pages/edit.tpl.php
@@ -1,5 +1,31 @@
-<h3>Administração de páginas</h3>
+<h3 class="mb-5">Edição de páginas</h3>
 
-//edição
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="pagesTitle">Título</label>
+        <input name="title" id="pagesTitle" type="text" clas="form-control" placeholder="Aqui vai o título da página..." required value="Página inicial">
+    </div>
 
-<a href="/admin">Ir para home</a>
+    <div class="form-group">
+        <label for="pagesUrl">URL</label>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text">/</span>
+            </div>
+            <input name="url" id="pagesUrl" type="text" class="form-control" 
+            placeholder="URL amigável, deixe em branco para informar a página inicial..." >
+        </div>
+    </div>
+
+    <div class="form-group">
+        <input id="pagesBody" type="hidden" name="body" value="<h3>Página inicial</h3><p>Esta é a página inicial do site</p>">
+        <trix-editor input="pagesBody"></trix-editor>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Salvar</button>
+
+</form>
+
+<hr>
+
+<a href="/admin/pages/1" class="btn btn-secondary">Ir para home</a>
