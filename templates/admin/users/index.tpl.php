ac3b34ce49518dfce2964f86dd5d933fad4baa4c
Yuri Magalhaes <yurirobertocm@gmail.com>
2018-11-30 12:30:26 -0300

    Template de listagem de usuário

---
+++ templates/admin/users/index.tpl.php
@@ -0,0 +1,25 @@
+<h3 class="mb-5">Administração de usuários</h3>
+
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>email</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>yurirobertocm@gmail.com</td>
+            <td class="text-right">
+                <a href="" class="btn btn-primary btn-sm">Ver</a>
+            </td>
+
+        </tr>
+
+    </tbody>
+
+</table>
+
+<a href="/admin/pages/users" class="btn btn-secondary">Novo</a>
