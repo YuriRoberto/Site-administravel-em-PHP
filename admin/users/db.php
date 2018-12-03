562b557e320cb578e5b40fe9cd22ba2e958a505c
Yuri Magalhaes <yurirobertocm@gmail.com>
2018-12-03 13:11:50 -0300

    Cadastro de usuários no banco

--- admin/users/db.php
+++ admin/users/db.php
@@ -1,17 +1,49 @@
 <?php 
 
+function user_get_data($redirectOnError){
+    $email = filter_input(INPUT_POST, 'email');
+    $password = filter_input(INPUT_POST, 'password');
+
+    if(is_null($email)){
+        flash('Informe o campo email', 'error');
+        header('location:' . $redirectOnError);
+        die();
+    }
+
+    return compact('email', 'password');
+
+}
+
 $users_all = function () use ($conn){
     $result = $conn->query('SELECT * FROM users');
     return $result->fetch_all(MYSQLI_ASSOC);
 
 };
 $users_view = function () use ($conn){
-
+    
 
 };
 
 $users_create = function () use ($conn){
+    $data = user_get_data('/admin/users/create');
 
+    $sql = 'insert into users (email, password, updated, created) VALUES (?, ?, NOW(), NOW())';
+
+    if(is_null($data['password'])){
+        flash('Informe o campo email', 'error');
+        header('location: /admin/users/create');
+        die();
+        
+    }
+
+    $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('ss', $data['email'], $data['password']);
+
+    flash('Salvo com sucesso', 'sucess');
+
+    return $stmt->execute();
 
 };
 
