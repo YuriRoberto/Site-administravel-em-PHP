2f89486feadb56cb20c527c8ed3ad86f9d95f1a3
Yuri Magalhaes <yurirobertocm@gmail.com>
2018-12-07 13:28:00 -0300

    Exibindo páginas do site

--- site/routes.php
+++ site/routes.php
@@ -1,9 +1,20 @@
 <?php
 
-if (resolve('/')){
-   render('site/home', 'site');
-} elseif (resolve('contato')){
-    render('site/contato', 'site');
-} else{
-    echo 'Página não encontrada';
-}
+require __DIR__ . '/../admin/pages/db.php';
+
+if  (resolve('/contato')){
+    $pages = $pages_all();
+    render('site/contato', 'site', compact('pages') );
+
+} elseif ($params = resolve('/(.*)')){
+    $pages = $pages_all();
+
+    foreach($pages as $page){
+        if($page['url'] == $params[1]){
+            break;
+        }
+    }
+
+    render('site/page', 'site', compact('pages', 'page'));
+
+} 
